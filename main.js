727b2d3b0d08f26c0fd8dff5bdec3aadcd86522d
EsrakFahim <efahim0656@gmail.com>
2024-10-19 12:41:30 +0600

    Add initial project files and structure

---
+++ main.js
@@ -0,0 +1,82 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#canvas"), // Canvas selector
+  antialias: true, // Enable antialiasing here
+});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // Retina display fix
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Enable damping (inertia)
+
+// HDRI Loader
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load(
+  './pond_bridge_night_1k.hdr', // Replace with the path to your HDRI file
+  (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.environment = texture;
+    // scene.background = texture;
+  },
+  undefined,
+  function (error) {
+    console.error('An error occurred loading the HDRI:', error);
+  }
+);
+
+// GLTF Loader
+const loader = new GLTFLoader();
+loader.load(
+  "./DamagedHelmet.gltf",
+  (gltf) => {
+    const model = gltf.scene;
+    scene.add(model);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+// Animation Loop
+function animate() {
+  window.requestAnimationFrame(animate); // Loop the render process
+
+  // Update controls
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+// Resize Handler to adjust canvas size on window resize
+window.addEventListener("resize", () => {
+  // Update camera aspect ratio and projection matrix
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  // Update renderer size
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Start animation
+animate();
